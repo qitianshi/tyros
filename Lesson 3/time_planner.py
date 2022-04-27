e650aecc4c9516d4c23c8634a02c986bdc5e9848
Wong Zenwei <zenweiyveltal@gmail.com>
2022-04-27 12:49:16 +0800

    Add solution for L3Q2 (#9)

--- Lesson 3/time_planner.py
+++ Lesson 3/time_planner.py
@@ -15,3 +15,15 @@
 # Write a program that allows me to input if I have school tomorrow, if I have
 # completed my homework and if I have friends online, then print what I should
 # do!
+
+
+school_tomorrow = input("Do I have school tomorrow? (Yes/No) ")
+completed_homework = input("Have I completed my homework? (Yes/No) ")
+friends_online = input("Do I have friends online (Yes/No) ")
+
+if (completed_homework == "Yes" or friends_online == "No") and school_tomorrow == "No":
+    print("Sleep early")
+if school_tomorrow == "Yes":
+    print("Homework")
+if friends_online == "Yes" and school_tomorrow == "No":
+    print("Play some games")
