878b6ed89aa476d2562e9077b6ecec72ff604311
qitianshi <65122416+qitianshi@users.noreply.github.com>
2022-05-07 16:58:13 +0800

    Add L5Q3 solutions

--- Lesson 5/compound_interest.py
+++ Lesson 5/compound_interest.py
@@ -8,3 +8,18 @@
 # interest per annum. Write a programme that takes in the original amount
 # deposited, and prints out the total amount of money in the account for each
 # of the next 10 years.
+
+
+money = int(input("Original deposit amount: "))
+this_year = 2022
+
+for i in range(10):
+
+    this_year += 1
+    money *= 1.001
+
+    print(this_year, money)
+
+
+# Try adding functionality to your code to handle additional deposits and
+# withdrawals every year, or to have the interest rate be changing.
